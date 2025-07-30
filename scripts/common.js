41d34a6864618328b3241e8599fc2f5f6c38ed44
Firebase Studio <noreply@firebase.studio>
2025-07-30 15:14:57 +0000

    Initialized workspace with Firebase Studio

---
+++ scripts/common.js
@@ -0,0 +1,6 @@
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzn_qTN6KjplFUBD6oWV9VvvNrtG_FmQ7cbd77Jre4HdAdb8TfGchdnDIg4vtfjlrFN4w/exec';
+
+const months = [
+    'Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec',
+    'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'
+];
